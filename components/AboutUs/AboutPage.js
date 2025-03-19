2d55a6254c5a84db211a4b4289f3af9bc3e6a90d
Raymond <32289508+Ubaton@users.noreply.github.com>
2025-03-19 17:03:55 +0200

    Update to Images extesions patch error fixed

--- components/AboutUs/AboutPage.js
+++ components/AboutUs/AboutPage.js
@@ -4,8 +4,8 @@ import React, { useState } from "react";
 import { Button } from "../ui/button";
 import Image from "next/image";
 import backgroundImage from "../../public/assets/background.png";
-import displayImage from "../../public/assets/img2.jpg";
-import displayImage2 from "../../public/assets/img7.jpg";
+import displayImage from "../../public/assets/img2.png";
+import displayImage2 from "../../public/assets/img7.png";
 
 const AboutPage = () => {
   const [isPaused, setIsPaused] = useState(false);
