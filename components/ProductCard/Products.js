25be094fb23777331c6a133f6076117751c3456b
3liantte <eliantte49@gmail.com>
2025-03-29 12:11:30 +0200

    images update

--- components/ProductCard/Products.js
+++ components/ProductCard/Products.js
@@ -6,19 +6,19 @@ import { useRouter } from "next/navigation";
 
 function Products() {
   const products = [
-    { id: 1, title: "Customised Fabrics", imageUrl: "/assets/customise.jpg" },
-    { id: 2, title: "Diamond Range", imageUrl: "/assets/diamond.jpg" },
-    { id: 3, title: "Domestic Range", imageUrl: "/assets/domestic.jpg" },
-    { id: 4, title: "Studio Explore", imageUrl: "/assets/explore.jpg" },
-    { id: 5, title: "The Great Outdoor", imageUrl: "/assets/outdoor.jpg" },
-    { id: 6, title: "The Great Plains", imageUrl: "/assets/plains.jpg" },
-    { id: 7, title: "Scape Range", imageUrl: "/assets/scape.jpg" },
-    { id: 8, title: "Slub Plain", imageUrl: "/assets/slub.jpg" },
-    { id: 9, title: "Studio Range", imageUrl: "/assets/studio.jpg" },
-    { id: 10, title: "Tailor Range", imageUrl: "/assets/tailor.jpg" },
-    { id: 11, title: "Valley Range", imageUrl: "/assets/valley.jpg" },
-    { id: 12, title: "Westcliff Range", imageUrl: "/assets/westcliff.jpg" },
-    { id: 13, title: "@Work Range", imageUrl: "/assets/workrange.jpg" },
+    { id: 1, title: "Customised Fabrics", imageUrl: "/assets/wallpaper/Customise.jpg" },
+    { id: 2, title: "Diamond Range", imageUrl: "/assets/wallpaper/DiamondRange.jpg" },
+    { id: 3, title: "Domestic Range", imageUrl: "/assets/wallpaper/Domestic.jpg" },
+    { id: 4, title: "Studio Explore", imageUrl: "/assets/wallpaper/StudioExplore.jpg" },
+    { id: 5, title: "The Great Outdoor", imageUrl: "/assets/wallpaper/GreatOutdoor.jpg" },
+    { id: 6, title: "The Great Plains", imageUrl: "/assets/wallpaper/GreatPlains.jpg" },
+    { id: 7, title: "Scape Range", imageUrl: "/assets/wallpaper/ScapeRange.jpg" },
+    { id: 8, title: "Slub Plain", imageUrl: "/assets/wallpaper/SlubPlain.jpg" },
+    { id: 9, title: "Studio Range", imageUrl: "/assets/wallpaper/StudioRange.jpg" },
+    { id: 10, title: "Tailor Range", imageUrl: "/assets/wallpaper/TailorRange.jpg" },
+    { id: 11, title: "Valley Range", imageUrl: "/assets/wallpaper/ValleyRange.jpg" },
+    { id: 12, title: "Westcliff Range", imageUrl: "/assets/wallpaper/WestcliffRange.jpg" },
+    { id: 13, title: "@Work Range", imageUrl: "/assets/wallpaper/WorkRange.jpg" },
   ];
 
   const router = useRouter();
