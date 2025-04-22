c7ebc05d7cbc1c5c537b4f2092a9f0e6e91a5d0a
3liantte <eliantte49@gmail.com>
2025-04-22 19:52:41 +0200

    finsihes page

---
+++ app/finishes/page.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Finishes = () => {
+  return (
+    <div className="text-center pt-24 text-gray-600 h-screen flex flex-col justify-center items-center">
+      <h5 className="uppercase tracking-[.15rem]">welcome to our Finishes Section!</h5>
+      <p className="italic text-base md:text-md leading-relaxed px-4 sm:px-8 lg:px-16 m-2">
+        Coming Soon..
+      </p>
+      {/* <hr className="inline-flex w-3/5 rounded" /> */}
+    </div>
+  );
+};
+
+export default Finishes;
