fa00f59082462bec950e963a08057207844634bf
3liantte <eliantte49@gmail.com>
2025-04-22 15:35:43 +0200

    product card update

--- components/ProductCard/Products.js
+++ components/ProductCard/Products.js
@@ -6,26 +6,25 @@ import { useRouter } from "next/navigation";
 
 function Products() {
   const products = [
-    { id: 1, title: "Customised Fabrics", imageUrl: "/assets/wallpaper/Customise.jpg" },
+    { id: 13, title: "@Work Range", imageUrl: "/assets/wallpaper/WorkDisplay.png" },
     { id: 2, title: "Diamond Range", imageUrl: "/assets/wallpaper/DiamondDisplay.png" },
-    { id: 3, title: "Domestic Range", imageUrl: "/assets/wallpaper/Domestic.jpg" },
-    { id: 4, title: "Studio Explore", imageUrl: "/assets/wallpaper/StudioExplore.jpg" },
-    { id: 5, title: "The Great Outdoor", imageUrl: "/assets/wallpaper/GreatOutdoor.jpg" },
-    { id: 6, title: "The Great Plains", imageUrl: "/assets/wallpaper/GreatPlains.jpg" },
     { id: 7, title: "Scape Range", imageUrl: "/assets/wallpaper/ScapeRange.jpg" },
     { id: 8, title: "Slub Plain", imageUrl: "/assets/wallpaper/SlubDisplay.png" },
+    { id: 4, title: "Studio Explore", imageUrl: "/assets/wallpaper/StudioExplore.jpg" },
     { id: 9, title: "Studio Range", imageUrl: "/assets/wallpaper/StudioRange.jpg" },
     { id: 10, title: "Tailor Range", imageUrl: "/assets/wallpaper/TailorRange.jpg" },
+    { id: 5, title: "The Great Outdoor", imageUrl: "/assets/wallpaper/GreatOutdoor.jpg" },
+    { id: 6, title: "The Great Plains", imageUrl: "/assets/wallpaper/GreatPlains.jpg" },
     { id: 11, title: "Valley Range", imageUrl: "/assets/wallpaper/ValleyDisplay.png" },
     { id: 12, title: "Westcliff Range", imageUrl: "/assets/wallpaper/WestcliffDisplay.png" },
-    { id: 13, title: "@Work Range", imageUrl: "/assets/wallpaper/WorkDisplay.png" },
+    { id: 1, title: "Create Your Own", imageUrl: "/assets/wallpaper/Customise.jpg" },
   ];
 
   const router = useRouter();
 
   const handleDetailsClick = (id, title) => {
     const product = products.find((p) => p.id === id);
-    if (product && product.imageUrl) {
+    if (product?.imageUrl) {
       router.push(
         `/collections/?title=${encodeURIComponent(
           title
@@ -44,8 +43,10 @@ function Products() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {products.map((items, index) => {
           return (
+            // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
             <div
               onClick={() => handleDetailsClick(items.id, items.title)}
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
               key={index}
               className="border rounded-xl p-4 cursor-pointer"
             >
