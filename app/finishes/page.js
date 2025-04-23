7c221975e9dc88ff2903976028584057831a29dc
3liantte <eliantte49@gmail.com>
2025-04-23 15:57:44 +0200

    finishes update

--- app/finishes/page.js
+++ app/finishes/page.js
@@ -1,13 +1,10 @@
 import React from "react";
+import EasyClean from "../../components/Finishes/EasyClean";
 
 const Finishes = () => {
   return (
     <div className="text-center pt-24 text-gray-600 h-screen flex flex-col justify-center items-center">
-      <h5 className="uppercase tracking-[.15rem]">welcome to our Finishes Section!</h5>
-      <p className="italic text-base md:text-md leading-relaxed px-4 sm:px-8 lg:px-16 m-2">
-        Coming Soon..
-      </p>
-      {/* <hr className="inline-flex w-3/5 rounded" /> */}
+      <EasyClean/>
     </div>
   );
 };
