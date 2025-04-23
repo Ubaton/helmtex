d677d08b89ac4f950e1fcee266d99afc4bbd24fa
3liantte <eliantte49@gmail.com>
2025-04-23 15:57:54 +0200

    EasyClean info added

---
+++ components/Finishes/EasyClean.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const EasyClean = () => {
+  return (
+    <div className="max-w-5xl mx-auto px-6 py-10 text-gray-800">
+      <h1 className="text-3xl font-bold mb-4 text-blue-700">EASYCLEAN®</h1>
+      <p>
+        EASY CLEAN is our water repeelent finish, your trusted solution for long-lasting, easy-t-maintain fabric care.
+      </p>
+
+      <h2 className="list-disc list-inside space-y-1 mb-6">Key Features</h2>
+      <ul>
+        <li>Water Repellent</li>
+        <li>Anti-Dust Mite Protection</li>
+        <li>Dirt Repellent</li>
+        <li>Anti-Mildew Properties</li>
+        <li>Stain Resistant</li>
+      </ul>
+    </div>
+  )
+}
+
+export default EasyClean
