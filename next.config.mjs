94981b7b3f8555680743daebf581d26f485bc639
3liantte <eliantte49@gmail.com>
2025-04-24 14:32:23 +0200

    trailingSlash: true added to ensure static pages are generated like /about/index.html instead of /about.html.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export",
+  trailingSlash: true,
   images: {
     domains: ["firebasestorage.googleapis.com"],
     unoptimized: true,
