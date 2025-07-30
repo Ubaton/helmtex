4fc0673b3afede19e1e58e7cec16eba2e5ff8297
3liantte <eliantte49@gmail.com>
2025-07-30 14:11:08 +0200

    blog update

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Nav from "../components/Navbar/Nav";
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
-// import { Toaster } from "../../components/ui/sonner";
+import { Toaster } from "../components/ui/sonner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -61,7 +61,7 @@ export default function RootLayout({ children }) {
         {children}
         <Analytics/>
         <SpeedInsights/>
-        {/* <Toaster /> */}
+        <Toaster/>
       </body>
     </html>
   );
